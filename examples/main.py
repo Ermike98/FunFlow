0cddda8e7d0eff7b89deb965a3d007763d1ac815
michelangelo saveriano <michelangelo.saveriano.1998@gmail.com>
2024-04-18 15:15:16 +0200

    Minor changes to main.py

--- examples/main.py
+++ examples/main.py
@@ -1,14 +1,14 @@
 # from src_old import Layer, Functional
 from flow import Functional, Model, Layer
-
+from pprint import pprint
 
 def f(x):
-    print("x:", x)
+    # print("x:", x)
     return f"f-x:({x})", f"f-x:({x})", f"f-x:({x})"
 
 
 def g(un):
-    print("un:", un)
+    # print("un:", un)
     return f"g-un:({un})"
 
 
@@ -17,8 +17,10 @@ def h(x, dos):
 
 
 def f_kwargs(**kwargs):
-    print(kwargs)
-    return f"kwargs:{kwargs}"
+    # print(kwargs)
+    return f"kwargs "
+
+
 
 
 # layer = Functional(f, debug=True, outputs=['output'], inputs="x", raw_inputs=False)
@@ -28,7 +30,7 @@ layer2 = Functional(f, debug=True, outputs=['un', 'dos', 'tres'], inputs="x")
 layer5 = Functional(f_kwargs, debug=True, outputs=['kwd'], inputs=['x', 'dos', 'un'])
 model = Model([layer2, layer5], debug=True, outputs=['x', 'un', 'kwd'])
 
-print(model(x=10))
+pprint(model(x=10))
 # layer = Functional(lambda x: x)
 # print(layer(10))
 # print(layer(10))
