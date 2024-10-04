dc14e8664a3e40873f2799ceb5a1cceab9be9000
michelangelo saveriano <michelangelo.saveriano.1998@gmail.com>
2024-10-04 16:52:12 +0200

    adapt Map even though it is not used

--- funflow/map.py
+++ funflow/map.py
@@ -1,32 +1,40 @@
 import itertools
 from typing import Callable
 from .layer import Layer
-from .template_utils import replace_multi_templates
+from .template_utils import replace_multi_templates, create_name_to_inputs_mapping
+from .templates import Template, TemplateValue
 
 
 class Map(Layer):
     def __init__(self,
                  func: Callable,
-                 inputs: list[str] = None,
-                 outputs: list[str] = None,
+                 inputs: str | list[str] | Template | list[Template] = None,
+                 outputs: str | list[str] | Template | list[Template] = None,
                  map_over: list[str] = None,
                  func_input_type: str = 'args',
                  func_output_type: str = 'tuple',
                  **kwargs):
         super().__init__(inputs=inputs, outputs=outputs, **kwargs, output_type="dict", call_type="kwargs")
         self.__func = func
-        self.__map_over = map_over
+        self.__map_over = map_over  # TODO - Add warning if name in map_over is not present in input_names/templates
         self.__func_input_type = func_input_type
         self.__func_output_type = func_output_type
 
+    def __get_map_over(self, tags):
+        pass
+
     def call(self, **kwargs):
+        input_template_values = list(map(TemplateValue, kwargs.keys()))
+        name_to_template_values = create_name_to_inputs_mapping(input_template_values)
+
         if self.__map_over is not None:
-            for template in self.__map_over:
-                assert template in self._template_values, (f"Trying to map over template {template} but {template} is "
-                                                           f"not in the input templates: {list(self._template_values.keys())}")
+            for input_name in self.__map_over:
+                assert input_name in name_to_template_values, (
+                    f"Trying to map over template {input_name} but {input_name} is "
+                    f"not in the input templates: {list(name_to_template_values.keys())}")
             map_over = self.__map_over
         else:
-            map_over = self._template_values.keys()
+            map_over = name_to_template_values.keys()
 
         excluded_templates = [template for template in self._template_values if template not in map_over]
 
@@ -38,10 +46,9 @@ class Map(Layer):
 
             input_names = replace_multi_templates(self.inputs, template_values_dict)
 
-            outputs = None
             if self.__func_input_type == "args":
                 outputs = self.__func(*(kwargs[input_name] for input_name in input_names))
-            elif self.__func_input_type == "kwargs":
+            else:  # self.__func_input_type == "kwargs"
                 outputs = self.__func(**{input_name: kwargs[input_name] for input_name in input_names})
 
             output_names = replace_multi_templates(self.outputs, template_values_dict)
