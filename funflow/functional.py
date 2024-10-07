fa8b2348bdbf2bec6596046dac8f900ec8bc6917
michelangelo saveriano <michelangelo.saveriano.1998@gmail.com>
2024-10-07 14:25:35 +0200

    improve default params for Functional layer

--- funflow/functional.py
+++ funflow/functional.py
@@ -8,8 +8,9 @@ class Functional(Layer):
                  func: Callable,
                  inputs: str | Template | list[Template | str] = None,
                  outputs: str | Template | list[Template | str] = None,
+                 call_type: str = "args",
                  **kwargs):
-        super().__init__(inputs=inputs, outputs=outputs, **kwargs)
+        super().__init__(inputs=inputs, outputs=outputs, call_type=call_type, **kwargs)
         self.__func = func
 
     def call(self, *args, **kwargs):
